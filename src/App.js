4b48615415737e8d188cfa32f2da687eb9d58684
felixatchur <liuzhengyangg@gmail.com>
2020-05-13 13:11:45 +1000

    [npm eject] eject config files in order to implement our own configurations. In ths case, babel-plugin-react-remove-properties is implemented in order to remove all the data-test attributes from components before production; [the first TDD component] finish the first TDD component App with tests in App.test.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  incrementCounter = () => {
+    this.setState((state) => {
+      return {
+        counter: state.counter + 1
+      };
+    });
+  }
+
   render() {
     return (
       <div data-test="component-app">
-        <h1>App</h1>
+        <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+        <button 
+          data-test="increment-button" 
+          onClick={this.incrementCounter}
+        >
+          Increment counter
+        </button>
       </div>
     );
   }
