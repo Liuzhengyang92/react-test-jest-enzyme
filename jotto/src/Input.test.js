835f295c73f984eb1bb90c0b0710ce2cae495835
felixatchur <liuzhengyangg@gmail.com>
2020-05-16 15:08:14 +1000

    complete Input component render test for success=true

--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -16,19 +16,28 @@ const setup = (initialState={}) => {
   // as we have store for Input, so the shallow retunrs a ContextProvider with Input as its child component
   // the second dive() will fetch the child component of Input
   const wrapper = shallow(<Input store={store}/>).dive().dive();
+  return wrapper;
   // console.log(wrapper.debug())
 }
 
 describe('render', () => {
   describe('word has not been guessed', () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = {success: false};
+      wrapper = setup(initialState)
+    });
     test('renders component without error', () => {
-
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
     });
     test('renders input box', () => {
-
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
     });
     test('renders submit button', () => {
-
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(1);
     });
   })
   describe('word has been guessed', () => {
