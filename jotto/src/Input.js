c31a5d6cd2ac60ff084c5e3dcec8a282b2cfdc84
felixatchur <liuzhengyangg@gmail.com>
2020-05-19 11:53:50 +1000

    Make tests and code for guessWord arg

--- jotto/src/Input.js
+++ jotto/src/Input.js
@@ -3,7 +3,28 @@ import { connect } from 'react-redux';
 
 import { guessWord } from './actions';
 
-class Input extends Component {
+export class UnconnectedInput extends Component {
+  /**
+   * @method constructor
+   * @param {object} props - Component props
+   * @returns {undefined} 
+   */
+  constructor(props) {
+    super(props);
+    // initialize state
+    this.state = {
+      currentGuess: null
+    }
+    this.submitGuessWord = this.submitGuessWord.bind(this);
+  }
+  submitGuessWord(evt) {
+    evt.preventDefault();
+    const guessedWord = this.state.currentGuess;
+
+    if (guessedWord && guessedWord.length > 0) {
+      this.props.guessWord(guessedWord);
+    }
+  }
   render() {
     const content = this.props.success 
     ? null 
@@ -13,12 +34,16 @@ class Input extends Component {
           data-test="input-box"
           className="mb-2 mx-sm-3"
           type="text"
-          placeholder="enter guess" 
+          placeholder="enter guess"
+          value={this.state.currentGuess}
+          onChange={(evt) => this.setState({ currentGuess: evt.target.value })}
         />
         <button 
           data-test="submit-button"
           className="btn btn-primary mb-2"
           type="submit"
+          // onClick={() => this.props.guessWord(this.state.currentGuess)}
+          onClick={(evt) => this.submitGuessWord(evt)}
         >
           Submit
         </button>
@@ -34,4 +59,4 @@ const mapStateToProps = ({ success }) => {
   return { success };
 }
 
-export default connect(mapStateToProps, { guessWord })(Input);
+export default connect(mapStateToProps, { guessWord })(UnconnectedInput);
--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { findByTestAttr, storeFactory } from '../test/testUtils';
-import Input from './Input';
+import Input, { UnconnectedInput } from './Input';
 
 /**
  * Factory function to create a ShallowWrapper for the Input component
@@ -73,4 +73,40 @@ describe('redux props', () => {
     const guessWordProp = wrapper.instance().props.guessWord;
     expect(guessWordProp).toBeInstanceOf(Function)
   })
+});
+
+
+describe('`guessWord` action creator call', () => {
+  let guessWordMock;
+  let wrapper;
+  const guessedWord = "train";
+
+  beforeEach(() => {
+    // set up mock for `guessWord`
+    guessWordMock = jest.fn();
+    const props = {
+      guessWord: guessWordMock
+    }
+
+    // set up app component with guessWordMock as the guessWord prop
+    wrapper = shallow(<UnconnectedInput {...props} />)
+
+    // add value to input box
+    wrapper.setState({ currentGuess: guessedWord })
+
+    // simulate button click
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    submitButton.simulate('click', { preventDefault() {} });
+  })
+
+  test('test `guessWord` calls when click the submit button', () => {
+    // check to see if mock ran
+    const guessWordCallCount = guessWordMock.mock.calls.length;
+    expect(guessWordCallCount).toBe(1);
+  })
+  test('calls `guessWord` with input value as argument', () => {
+    console.log(guessWordMock.mock.calls);
+    const guessWordArg = guessWordMock.mock.calls[0][0];
+    expect(guessWordArg).toBe(guessedWord);
+  });
 });
