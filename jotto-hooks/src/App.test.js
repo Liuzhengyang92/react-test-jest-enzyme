895b759456c011e47426507990ab78cd91a14eb9
felixatchur <liuzhengyangg@gmail.com>
2020-05-19 17:37:24 +1000

    Implement hookActions in actions folder; apply useReducer in App component and call the functions in hookActions

--- jotto-hooks/src/App.test.js
+++ jotto-hooks/src/App.test.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
+import hookActions from './actions/hookActions';
+
+const mockGetSecretWord = jest.fn();
+
 /**
  * Setup function for app component
- * @returns {ShallowWrapper}
+ * @returns {ReactWrapper}
  */
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  // use mount, because useEffect not called on `shallow`
+  // https://github.com/airbnb/enzyme/issues/2086
+  return mount(<App />);
 }
 
 it('App renders without error', () => {
@@ -16,3 +25,12 @@ it('App renders without error', () => {
   const component = findByTestAttr(wrapper, 'component-app');
   expect(component.length).toBe(1);
 });
+
+describe('getSescretWord calls', () => {
+  test('getSecretWord gets called on App mount', () => {
+    setup();
+
+    // check to see if secret word was updated
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
+})
